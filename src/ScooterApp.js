e53731ffbbbba09b77bef6d89de13d32fd21ca35
Huzaif <huzaifsaiyed620@gmail.com>
2023-04-28 14:41:24 +0100

    boiler plate

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,8 +1,36 @@
 const User = require('./User')
 const Scooter = require('./Scooter')
+const User = require('./User')
 
 class ScooterApp {
   // ScooterApp code here
-}
+  constructor(stations,registeredUsers){
+    this.stations = stations
+    this.registeredUsers = registeredUsers
+  }
+  registerUser(username,password,age){
+    const result = this.registeredUsers.some(obj => obj[username] === password)
+    if(result){
+      throw Error('User is already registered')
+    }else if(age<18){
+      throw Error('User is under 18 and cannot register')
+    }
+    }
+    loginUser(username,password){
+      const result = this.registeredUsers.some(obj => obj[username] === password)
+      const User = new User()
+      User.login(result)
+
+    }
+
+
+    logoutUser(username){
+      const result = this.registeredUsers.some(obj => obj[username] === password)
+
+    }
+
+
+  }
+
 
 module.exports = ScooterApp
--- src/User.js
+++ src/User.js
@@ -1,5 +1,31 @@
-class User {
+const { createTestScheduler } = require("jest")
+const ScooterApp = require("./ScooterApp")
+
+class User extends ScooterApp {
   // User code here
+  constructor(username,password,age,loggedIn){
+    this.username = username
+    this.password = password
+    this.age = age
+    this.loggedIn = loggedIn
+  }
+  login(password){
+    if (password){
+      this.loggedIn = true
+    }else{
+      throw Error('Incorrect password')
+    }
+}
+
+  logout(result){
+    if(result){
+      this.loggedIn = false
+    } else{
+      
+    }
+
+  }
+
 }
 
 module.exports = User
