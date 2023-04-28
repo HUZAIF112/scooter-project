7327012cb9b1a7a2008090e1e13fb8c9fce733f9
Huzaif <huzaifsaiyed620@gmail.com>
2023-04-28 15:08:24 +0100

    updated

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,29 @@
 class Scooter{
   // scooter code here
+  constructor(station,user,serial,nextSerial,charge,isBroken){
+    this.station = station
+    this.user = user
+    this.serial = serial
+    this.nextSerial = nextSerial
+    this.charge = charge
+    this.isBroken = isBroken
+  }
+  rent(user){
+    if(this.charge >20 && this.isBroken == false){
+      this.station = null
+      this.user = user
+    } else if(this.charge < 20){
+      throw Error('Scooter needs charge')
+    }else{
+      throw Error('Scooter is broken')
+    }
+  }
+
+  dock(station){
+    this.user = null
+    this.station = station
+
+  }
 }
 
 
